31f14a089d7c793f15d2699f4d15cc45b90a458e
David Pinchen <davidpinchen@gmail.com>
2022-07-06 22:46:09 +0100

    Boilerplate removal and firebase setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBvaVmCk31IcRiUHwTH5dE-RN0zdOol18I",
+  authDomain: "fir-dev-bae24.firebaseapp.com",
+  projectId: "fir-dev-bae24",
+  storageBucket: "fir-dev-bae24.appspot.com",
+  messagingSenderId: "415811267911",
+  appId: "1:415811267911:web:a326f47ffb29f70d143062",
+  measurementId: "G-4G6368RKNB",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
