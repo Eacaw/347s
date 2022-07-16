71c12737e80c5813d9375454fd463f7a59fcdf8e
David Pinchen <davidpinchen@gmail.com>
2022-07-16 22:12:07 +0100

    Update feed to use firebase collection

--- src/Pages/Feed/Feed.js
+++ src/Pages/Feed/Feed.js
@@ -1,7 +1,33 @@
-import React from "react";
+import {
+  collection,
+  getFirestore,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import MobileRecipeCard from "./Components/MobileRecipeCard";
 
 function Feed() {
-  return <div style={{ height: "200px", marginTop: "200px" }}>Feed</div>;
+  const db = getFirestore();
+
+  const recipeObjRef = collection(db, "Recipes");
+  const q = query(recipeObjRef, orderBy("Contributor.Date", "asc"));
+  const [recipeObjects] = useCollectionData(q);
+
+  return (
+    <div className="flex-center top-padding-navbar">
+      <div className="center-align-question-cards">
+        {recipeObjects &&
+          recipeObjects.slice(0).map((recipeObj) => {
+            return recipeObj ? (
+              <MobileRecipeCard key={recipeObj.uid} recipeData={recipeObj} />
+            ) : null;
+          })}
+      </div>
+    </div>
+  );
 }
 
 export default Feed;
