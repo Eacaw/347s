a733234067057a441e1554f325d6a3932b7314cc
Eacaw <davidpinchen@gmail.com>
2022-08-30 18:20:09 +0100

    Remove redundant import

--- pages/eula.js
+++ pages/eula.js
@@ -1,13 +1,11 @@
-import React from react;
-
 export default function Eula() {
   return (
     <main>
       <h1>End User License Agreement</h1>
       <p>
-        This End User License Agreement (EULA) is a legal agreement between
-        you and 347s. This EULA agreement governs your acquisition and use of
-        our software (Software) directly from 347s or indirectly through a
+        This End User License Agreement (EULA) is a legal agreement between you
+        and 347s. This EULA agreement governs your acquisition and use of our
+        software (Software) directly from 347s or indirectly through a
         manufacturer (Manufacturer) of the Software.
       </p>
       <p>
@@ -15,15 +13,15 @@ export default function Eula() {
         installation process and using the Software. It provides a license to
         use the Software and contains warranty information and liability
         disclaimers. If you register for a free trial of the Software, this EULA
-        agreement will also govern that trial. By clicking accept or
-        installing and/or using the Software, you are confirming your acceptance
-        of the Software license and agreeing to become bound by the terms of
-        this EULA agreement. If you are entering into this EULA agreement on
-        behalf of a company or other legal entity, you represent that you have
-        the authority to bind such entity to these terms and conditions. If you
-        do not have such authority or if you do not agree with the terms and
-        conditions of this EULA agreement, do not install or use the Software,
-        and you must not accept this EULA agreement.
+        agreement will also govern that trial. By clicking accept or installing
+        and/or using the Software, you are confirming your acceptance of the
+        Software license and agreeing to become bound by the terms of this EULA
+        agreement. If you are entering into this EULA agreement on behalf of a
+        company or other legal entity, you represent that you have the authority
+        to bind such entity to these terms and conditions. If you do not have
+        such authority or if you do not agree with the terms and conditions of
+        this EULA agreement, do not install or use the Software, and you must
+        not accept this EULA agreement.
       </p>
       <p>
         This EULA agreement shall apply only to the Software supplied by the
