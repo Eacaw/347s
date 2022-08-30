6429d0fb8be91f9ca2c6ecf9929861a962cbdfc0
Eacaw <davidpinchen@gmail.com>
2022-08-30 18:17:33 +0100

    Add EULA page

---
+++ pages/eula.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+export default function Eula() {
+  return (
+    <main>
+      <h1>End User License Agreement</h1>
+      <p>
+        This End User License Agreement ("EULA") is a legal agreement between
+        you and 347s. This EULA agreement governs your acquisition and use of
+        our software ("Software") directly from 347s or indirectly through a
+        manufacturer ("Manufacturer") of the Software.
+      </p>
+      <p>
+        Please read this EULA agreement carefully before completing the
+        installation process and using the Software. It provides a license to
+        use the Software and contains warranty information and liability
+        disclaimers. If you register for a free trial of the Software, this EULA
+        agreement will also govern that trial. By clicking "accept" or
+        installing and/or using the Software, you are confirming your acceptance
+        of the Software license and agreeing to become bound by the terms of
+        this EULA agreement. If you are entering into this EULA agreement on
+        behalf of a company or other legal entity, you represent that you have
+        the authority to bind such entity to these terms and conditions. If you
+        do not have such authority or if you do not agree with the terms and
+        conditions of this EULA agreement, do not install or use the Software,
+        and you must not accept this EULA agreement.
+      </p>
+      <p>
+        This EULA agreement shall apply only to the Software supplied by the
+        Manufacturer herewith regardless of whether other software is referred
+        to as the Software within the context of this EULA agreement. The terms
+        also apply to any updates, supplements, Internet-based services, and
+        support services for the Software, unless other terms accompany those
+        items on delivery of the Software. If so, those terms apply. This EULA
+        agreement shall be governed by the laws of the State of New York without
+        regard to conflict of law provisions.
+      </p>
+      <p>
+        <strong>
+          <u>Terms and Definitions</u>
+        </strong>
+      </p>
+      <p>
+        <strong>License Grant</strong>
+      </p>
+      <p>
+        Subject to the terms of this EULA agreement, the Manufacturer hereby
+        grants you a personal, non-transferable, non-exclusive licence to use
+        the Software for the purposes of education and research.
+      </p>
+    </main>
+  );
+}
