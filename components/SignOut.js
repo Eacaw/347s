83e209ffdcda48af158277c594221b3e5f1f0b42
Eacaw <davidpinchen@gmail.com>
2022-08-30 18:17:06 +0100

    Add timeout to signout button

--- components/SignOut.js
+++ components/SignOut.js
@@ -4,7 +4,14 @@ import { auth } from "../lib/firebase";
 // Sign out button
 export default function SignOutButton() {
   return (
-    <button className="btn-signout" onClick={() => signOut(auth)}>
+    <button
+      className="btn-signout"
+      onClick={() => {
+        setTimeout(() => {
+          signOut(auth);
+        }, 250);
+      }}
+    >
       Sign out
     </button>
   );
