d3a19cdae1cc93ea044bfead81856edb3de882d7
Eacaw <davidpinchen@gmail.com>
2022-08-30 18:15:53 +0100

    Remove redundant meta tags

--- components/Metatags.js
+++ components/Metatags.js
@@ -7,15 +7,8 @@ export default function Metatags({
   return (
     <Head>
       <title>{title}</title>
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:site" content="@347s21" />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      {/* <meta name="twitter:image" content={image} /> */}
-
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      {/* <meta property="og:image" content={image} /> */}
     </Head>
   );
 }
