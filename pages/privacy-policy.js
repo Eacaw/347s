c8f756060e4b41ab7cd1a2d9e2a16c79ab230ed8
Eacaw <davidpinchen@gmail.com>
2022-08-30 17:18:21 +0100

    Add privacy policy page

---
+++ pages/privacy-policy.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+export default function PrivacyPolicy() {
+  return (
+    <main>
+      <h1>Privacy Policy</h1>
+      <p>
+        This privacy policy has been compiled to better serve those who are
+        concerned with how their 'Personally identifiable information' (PII) is
+        being used online. PII, as used in US privacy law and information
+        security, is information that can be used on its own or with other
+        information to identify, contact, or locate a single person, or to
+        identify an individual in context. Please read our privacy policy
+        carefully to get a clear understanding of how we collect, use, protect
+        or otherwise handle your Personally Identifiable Information in
+        accordance with our website.
+      </p>
+      <h2>
+        What personal information do we collect from the people that visit our
+        blog, website or app?
+      </h2>
+      <p>
+        When ordering or registering on our site, as appropriate, you may be
+        asked to enter your name, email address or other details to help you
+        with your experience.
+      </p>
+      <h2>When do we collect information?</h2>
+      <p>
+        We collect information from you when you register on our site, place an
+        order or enter information on our site.
+      </p>
+      <h2>How do we use your information?</h2>
+      <p>
+        We may use the information we collect from you when you register, make a
+        purchase, sign up for our newsletter, respond to a survey or marketing
+        communication, surf the website, or use certain other site features in
+        the following ways:
+      </p>
+      <ul>
+        <li>
+          To personalize your experience and to allow us to deliver the type of
+          content and/or service that you request.
+        </li>
+        <li>To improve our website in order to better serve you.</li>
+        <li>
+          To send periodic emails regarding your order or other products and
+          services.
+        </li>
+      </ul>
+      <h2>How do we protect your information?</h2>
+      <p>
+        We do not use vulnerability scanning and/or scanning to PCI standards.
+      </p>
+      <p>
+        We only provide articles and information. We never ask for personal or
+        private information like passwords or credit cards. You are responsible
+        for maintaining the confidentiality of your private information.
+      </p>
+    </main>
+  );
+}
