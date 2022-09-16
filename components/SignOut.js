40e77333f2f042bbe3fc15fe88bf2bdb48b89b0c
David Pinchen <davidpinchen@gmail.com>
2022-09-16 17:13:02 +0100

    Fix signout bug

--- components/SignOut.js
+++ components/SignOut.js
@@ -1,12 +1,16 @@
 import { signOut } from "firebase/auth";
+import { Router, useRouter } from "next/router";
 import { auth } from "../lib/firebase";
 
 // Sign out button
 export default function SignOutButton() {
+  const router = useRouter();
+
   return (
     <button
       className="btn-signout"
       onClick={() => {
+        router.push("/");
         setTimeout(() => {
           signOut(auth);
         }, 250);
